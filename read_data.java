63a6d5ad84dfb68079651dc0b4eae65dea43e9e2
Lingxiang Yu <lingxiangyu@Shasibill-2.local>
2017-03-07 04:07:13 -0500

    init read data java  version

---
+++ read_data.java
@@ -0,0 +1,19 @@
+// Read data 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class read_data {
+	public static void main(String[] args) throws FileNotFoundException{
+
+		Scanner in = new Scanner(new File(args[0]));
+		PrintWriter out = new PrintWriter("ES.asc");
+
+
+	}
+
+
+}
