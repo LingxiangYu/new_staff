70c7b88a89a7f05f65eedf75db167c05a175dba7
Lingxiang Yu <lingxiangyu@dyn-160-39-162-150.dyn.columbia.edu>
2017-03-06 20:43:57 -0500

    py final ver

--- HW1_P11_5360.py
+++ HW1_P11_5360.py
@@ -1,4 +1,16 @@
 # HW1_P11_5360
+######################################################
+# none of the various versions of linear regression  #
+# give me the same coeffes or statistics inference.  #
+# The reason is that there multi-colinear variables  #
+# and they couldn't be removed by the codes below.   #
+# On the contrary, R can remove colinear problem     #
+# automatically, which is awesome. Hence, in the     #
+# future, it's possible that I use python to modify  #
+# data or run loops and then use R to gain satistics #
+# analysis.                                          #
+######################################################
+
 from astropy.io import ascii
 import numpy as np 
 import pandas as pd 
@@ -44,4 +56,30 @@ results = sm.OLS(np.log10(sigma_tau), np.log10(tau)).fit()
 from scipy import stats
 results1 = stats.linregress(sigma_tau, tau)
 results2 = stats.linregress(np.log10(sigma_tau), np.log10(tau))
+
+## write the linear reg 
+sigma_tau = np.reshape(sigma_tau, (len(sigma_tau), 1))
+tau = np.reshape(tau, (len(tau),1))
+
+from sklearn import linear_model
+sigma_tau_train = sigma_tau[0:int(len(sigma_tau)*0.8)]
+sigma_tau_test = sigma_tau[int(len(sigma_tau)*0.8):]
+sigma_tau_train = np.reshape(sigma_tau_train, (len(sigma_tau_train),1))
+sigma_tau_test = np.reshape(sigma_tau_test, (len(sigma_tau_test),1))
+
+tau_train = tau[0:int(len(tau)*0.8)]
+tau_test = tau[int(len(tau)*0.8):]
+tau_train = np.reshape(tau_train, (len(tau_train), 1))
+tau_test = np.reshape(tau_test, (len(tau_test), 1))
+
+results3 = linear_model.LinearRegression()
+results3.fit(sigma_tau_train, tau_train)
+results4 = linear_model.LinearRegression()
+results4.fit(np.log10(sigma_tau_train), np.log10(tau_train))
+
+print('Coefficients of linlin: \n', results3.coef_)
+print("Mean squared error of linlin: %.2f" % np.mean((results3.predict(sigma_tau_test) - tau_test) ** 2))
+
+print 'Coeffs of loglog ', results4.coef_
+print 'R Squared of loglog: ', np.mean((results4.predict(np.log10(sigma_tau_test)) - np.log10(tau_test)) ** 2)
 
